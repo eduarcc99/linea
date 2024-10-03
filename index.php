f1b42ee62f2efc0a0216bdf9ae98046cc3ff39dd
eduarcc99 <eduarcastilloculquerricra@gmail.com>
2024-10-02 22:40:55 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Shift + Alt + F. -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href=resources\resources\css\bootstrap.min.css>
+    <link rel="stylesheet" href="resources\resources\css\bootstrap-icons\font\bootstrap-icons.min.css">
+    <link rel="stylesheet" href="resources\resources\css\main.css">
+</head>
+
+<body class="px-3">
+    <!-- encabezado -->
+    <div class="row pt-5">
+        <div class="col-sm-12 col-md-6">
+            <h1 class="text-center">EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right   pr-5 ">SOLUCIONES MEMORABLES </p>
+
+        </div>
+    </div>
+    <!-- fin del encabesado -->
+    <div class="row">
+        <div class="col-sm-12 "></div>
+        <img class="w-100" style="height: 440px;" src="resources/resources/img/baner01.PNG" alt="">
+        <link rel="stylesheet" href="./resources/css/boostrap">
+
+    </div>
+
+    <div class="conatiner  mt-5">
+        <h1>SOLUCIONES CREATIVAS</h1>
+        <p>Sin importar cuál sea tu negocio, nuestro equipo puede hacer todo desde cero.</p>
+
+        <div class="row mt-4">
+            <div class="col-sm-12 col-md-4 ">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/resources/img/baner01.PNG" alt="">
+                        <h5 class="h5 mt-2"><i class="bi bi-pencil-square"></i>DISEÑO</h5>
+                        <P>CREAMOS DISEÑOS SEGUN TUS NESECIDADES</P>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 ">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/resources/img/idea_02.PNG" alt="">
+                        <h5 class="h5 mt-2"><i class="bi bi-flower3"></i>DISEÑO</h5>
+                        <P>CREAMOS DISEÑOS SEGUN TUS NESECIDADES</P>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 ">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="resources/resources/img/idea_03.PNG" alt="">
+                        <h5 class="h5 mt-2"><i class="bi bi-file-plus"></i>DISEÑO</h5>
+                        <P>CREAMOS DISEÑOS SEGUN TUS NESECIDADES</P>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <footer class="bg-wine">
+        <p class="p-3">Edificio Las Hadas. Guadalajara www sitioincreible.mx (33) 3214 6547</p>
+    </footer>
+
+</body>
+
+</html>
